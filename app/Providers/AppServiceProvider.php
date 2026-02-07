60b27225d60e965a3c029d9a320b42e7854579f5
webzyro <gmbworkswebzyro@gmail.com>
2026-02-07 17:01:49 +0530

    fix changes suggested by coderabbit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         View::composer('components.navbar', function ($view) {
-            $view->with('services', Service::all());
+            $view->with('services', Service::where('is_active', true)->get());
         });
     }
 }
